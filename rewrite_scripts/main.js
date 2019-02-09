035b67ffc099bc45469aa964cc2de01fe4cb3650
hiroshi-ho <h.hiroshi.nlp@gmail.com>
2019-02-09 20:01:37 +0900

    success, but still some bug bug1: reading time autimatically, not intended bug2: color changed for each 1 (0.5?) sec todo: color icon at each avator

--- rewrite_scripts/main.js
+++ rewrite_scripts/main.js
@@ -1,10 +1,28 @@
 // If class containts 'avatar', just rewrite.
-
 // rewrite for each tag https://symfoware.blog.fc2.com/blog-entry-1515.html
 
-$(function(){
-    $("img").each(function(){
+//TODO : deal with newly loaded tweets
 
+function choose_at_random(arrayData) {
+  var arrayIndex = Math.floor(Math.random() * arrayData.length);
+  return arrayData[arrayIndex]; }
+
+  //deal with newly loaded tweets
+function DOMModificationHandler(){
+  $(this).unbind('DOMSubtreeModified.event1');
+  setTimeout(function(){
+
+    modify();
+    $('#timeline').bind('DOMSubtreeModified.event1',DOMModificationHandler);
+  },1000);
+}
+$('#timeline').bind('DOMSubtreeModified.event1',DOMModificationHandler);
+
+
+
+
+function modify(){
+  $("img").each(function(){
       // https://stackoverflow.com/questions/3196613/jquery-determine-if-ul-has-class-or-another-one
 
         if ($('img').hasClass('Avatar Avatar--size32') ||
@@ -13,19 +31,33 @@ $(function(){
            $('img').hasClass('avatar js-action-profile-avatar ') ||
            $('img').hasClass('top-timeline-tweet-box-user-image avatar size32') ||
            $('img').hasClass('avatar js-action-profile-avatar ') ||
-           $('img').hasClass('avatar js-action-profile-avatar') ||
+           $('img').hasClass('js-action-profile-avatar') ||
            $('img').hasClass('MomentUserByline-avatar') ||
            $('img').hasClass('ProfileAvatar-image') ||
            $('img').hasClass('ProfileCardMini-avatarImage')
-        ) {
+        ){
           // https://peacepopo.net/blog-entry-161.html
-          var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-          // Rewrite image to random colors
-          // http://shanabrian.com/web/jquery/image01.php
 
-          var stylechar = "background-color:" + hue
-           $("img").removeAttr('src');
+          // http://shanabrian.com/web/jquery/image01.php
+          var randomColor0 = "#" + Math.floor(Math.random() * 16777215).toString(16);          // Rewrite image to random colors
+          var randomColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16);          // Rewrite image to random colors
+          var randomColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);          // Rewrite image to random colors
+          var randomColor3 = "#" + Math.floor(Math.random() * 16777215).toString(16);          // Rewrite image to random colors
+          var randomColor4 = "#" + Math.floor(Math.random() * 16777215).toString(16);          // Rewrite image to random colors
+          var randomColor5 = "#" + Math.floor(Math.random() * 16777215).toString(16);          // Rewrite image to random colors
+          var randomColor6 = "#" + Math.floor(Math.random() * 16777215).toString(16);          // Rewrite image to random colors
+
+          var coloardata= [randomColor0, randomColor1, randomColor2,randomColor3]
+          var randomColor = choose_at_random(coloardata)
+          var stylechar = "background-color:" + randomColor
            $("img").attr('style',stylechar);
+           $("img").attr('src','skelton.png');
+           $("img").attr('alt','');
+
+           // https://medium.com/@aidobreen/fixing-twitter-with-a-chrome-extension-1f53320f5a01
+           return true
+
       }
     });
-});
+
+  }
