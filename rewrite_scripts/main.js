a36b56768430d3be7f9517cb083e150551f9ab89
hiroshi-ho <h.hiroshi.nlp@gmail.com>
2019-02-09 12:28:21 +0900

    add avatarimagerewriter to text

---
+++ rewrite_scripts/main.js
@@ -0,0 +1,39 @@
+// If class containts 'avatar', just rewrite.
+
+// rewrite for each tag https://symfoware.blog.fc2.com/blog-entry-1515.html
+
+$(function(){
+    $("img").each(function(){
+        var value = $(this).html();
+
+        var CLASSES = [
+          'avatar',
+          'Avatar',
+          'js-action-profile-avatar',
+          'MomentUserByline-avatar',
+          'ProfileAvatar-image',
+          'ProfileCardMini-avatarImage'
+        ];
+
+      // https://stackoverflow.com/questions/3196613/jquery-determine-if-ul-has-class-or-another-one
+
+        if ($('img').hasClass('avatar') ||
+           $('img').hasClass('Avatar') ||
+           $('img').hasClass('js-action-profile-avatar') ||
+           $('img').hasClass('MomentUserByline-avatar') ||
+           $('img').hasClass('ProfileAvatar-image') ||
+           $('img').hasClass('ProfileCardMini-avatarImage') ||
+        ) {
+          // https://peacepopo.net/blog-entry-161.html
+          var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+          
+          // Rewrite image to random colors
+          // http://shanabrian.com/web/jquery/image01.php
+           $("img").html("test");
+
+      }
+
+
+
+    });
+});
